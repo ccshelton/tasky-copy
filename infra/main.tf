9d038852601c64e882726d52eb7c46d06867901f
Curtis Shelton <curtisshelton@Curtiss-MacBook-Pro.local>
2025-04-22 19:35:02 -0500

    Add terraform files for CI/CD

---
+++ infra/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "state" {
+  bucket = var.state_bucket_name
+  acl    = "private"
+}
